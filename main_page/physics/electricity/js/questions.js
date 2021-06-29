8b2e06bdab0fa1af55fb442286bf72de48ecc903
mobicanteen <mobicanteen@gmail.com>
2021-06-29 23:37:54 +0530

    first commit

---
+++ main_page/physics/electricity/js/questions.js
@@ -0,0 +1,113 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Which of the given is the SI Unit of Electric Current?",
+    answer: "Ampere",
+    options: [
+      "Ohm",
+      "Ampere",
+      "Volt",
+      "Faraday"
+    ]
+  },
+    {
+    numb: 2,
+    question: "A fuse wire is inserted in which wire?",
+    answer: "Live wire ",
+    options: [
+      "Live wire ",
+      "In the neutral wire ",
+      "In the earth wire",
+      "May be connected in any line"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The rate of flow of an electric charge is known as :",
+    answer: "Electric current",
+    options: [
+      "Electric Potential",
+      "Electric conductance",
+      "Electric current",
+      "None of these"
+    ]
+  },
+    {
+    numb: 4,
+    question: "The amount of work done in joules, when one unit electric charge moves from one point to another point in an electric circuit is called :",
+    answer: "Potential Difference",
+    options: [
+      "Electric Current",
+      "Electric Resistance",
+      "Electric Conductance",
+      "Potential Difference"
+    ]
+  },
+    {
+    numb: 5,
+    question: "The relation between potential difference (V) and current (I) is",
+    answer: "V2 α I2 ",
+    options: [
+      " V α I2",
+      "V α 1/I",
+      "V2 α I",
+      "V2 α I2 "
+    ]
+  },
+  {
+    numb: 6,
+    question: "The relation between potential difference (V) and current (I) was discovered by :",
+    answer: "Ohm",
+    options: [
+      " Newton",
+      "Ampere",
+      "Ohm",
+      " Volta"
+    ]
+  },
+  {
+    numb: 7,
+    question: "On which of the given resistance does not depend :",
+    answer: " Density",
+    options: [
+      " Length of conductor",
+      " Area of cross-section",
+      "Temperature",
+      " Density"
+    ]
+  },
+  {
+    numb: 8,
+    question: "In parallel combination of electrical appliances, total electrical power",
+    answer: "Remain same",
+    options: [
+      " Increase",
+      " Decrease",
+      " Does not change",
+      "Remain same"
+    ]
+  },
+  {
+    numb: 9,
+    question: "How the charge will flow?",
+    answer: "By applying potential difference",
+    options: [
+      " By heating them",
+      "By applying potential difference",
+      " Both",
+      "None of these"
+    ]
+  },
+  {
+    numb: 10,
+    question: "In SI unit, JC⁻¹ is equal to",
+    answer: " Newton's law",
+    options: [
+      " Newton's law",
+      "  Volt",
+      " Pascal",
+      "Omega"
+    ]
+  },
+  
+];
